ca050a0c35335a994c1cfe563ea58616366558cf
Anis <anis.moubarik@gmail.com>
2016-10-19 13:56:04 +0300

    started on the isnirequest conversion

--- mrc2isnimrc.py
+++ mrc2isnimrc.py
@@ -1,6 +1,6 @@
 import logging
-from pymarc import MARCReader, Record, Field
-import sys
+from pymarc import MARCReader, Record, Field, XMLWriter
+import sys, os, pprint
 
 
 class MARC21ToISNIMARC:
@@ -17,13 +17,14 @@ class MARC21ToISNIMARC:
 
         :param inputfilename: input file location
         :param outputfilename: output file location
+        :param skip: records with this field
         :param countrycode: ISO Alpha-2 countrycode
     """
 
 
-    def __init__(self, inputfilename, outputfilename, countrycode):
+    def __init__(self, inputfilename, countrycode, skip=None):
+        self.skip = skip
         self.infile = inputfilename
-        self.outfile = outputfilename
         self.countrycode = countrycode
 
     def __str__(self):
@@ -32,19 +33,21 @@ class MARC21ToISNIMARC:
         :return str: String representation of the object
         """
         return "Converting file %s to %s, <%s object at %s, %s bytes>" % (self.infile, self.outfile, self.__class__.__name__,
-                                                              hex(id(self)), self.__sizeof__())
+                                                                          hex(id(self)), self.__sizeof__())
 
     def __sizeof__(self):
         return sys.getsizeof(MARC21ToISNIMARC)
 
-    def convert(self):
+    def convert2Bin(self, outfile):
         """
         This method opens the specified files (input and output), reads infile, converts it and writes it into the outfile
+        :param outputfilename:
         """
-        logging.info("Starting conversion...")
+        logging.info("Starting mrc to mrc conversion...")
+
         with open(self.infile, 'rb') as fh:
             reader = MARCReader(fh, force_utf8=True, to_unicode=True)
-            out = open(self.outfile, 'wb')
+            out = open(outfile, 'wb')
             i = 0
             for record in reader:
                 logging.info("Converting record.")
@@ -52,13 +55,58 @@ class MARC21ToISNIMARC:
                 sys.stdout.flush()
                 if i % 5 == 0:
                     print("\rConverting", end="")
-                r = self.make_record(record)
+                r = self.makeRecord(record)
                 out.write(r.as_marc())
                 i += 1
             out.close()
             print("\rConversion done.")
 
-    def make_record(self, record):
+    def convert2XML(self, outfile):
+        """
+        Converts mrc bin format to isni xml marc format
+        :param outputfilename:
+        """
+        logging.info("Starting mrc to xml conversion...")
+        with open(self.infile, 'rb') as fh:
+            reader = MARCReader(fh, force_utf8=True, to_unicode=True)
+            out = XMLWriter(open(outfile, 'wb'))
+            i = 0
+            for record in reader:
+                logging.info("Converting record.")
+                sys.stdout.write(".")
+                sys.stdout.flush()
+                if i % 5 == 0:
+                    print("\rConverting", end="")
+                r = self.makeRecord(record)
+                out.write(r)
+                i += 1
+            out.close()
+            print("\rConversion done.")
+
+    def convert2ISNIRequest(self, dirname):
+        """
+
+        :param dirname:
+        """
+        if self.skip:
+            print("Skipping records with %s field" % self.skip)
+        logging.info("Starting mrc to isni request conversion...")
+        pp = pprint.PrettyPrinter(indent=2)
+        with open(self.infile, 'rb') as fh:
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
+            reader = MARCReader(fh, force_utf8=True, to_unicode=True)
+            for record in reader:
+                if any(f.tag == self.skip for f in record.fields):
+                    continue
+                logging.info("Converting record.")
+                r = self.makeIsniRequest(record)
+                pp.pprint(r)
+            print("Conversion done")
+
+
+
+    def makeRecord(self, record):
         """
         Converts the record the pymarcs MARCReader has read to ISNIMARC format.
         Takes a whole record as a parameter and returns the converted record.
@@ -67,7 +115,6 @@ class MARC21ToISNIMARC:
         """
         newrecord = Record(force_utf8=True, to_unicode=True)
         for field in record.fields:
-
             if field.tag == '024':
                 newrecord.add_field(Field(tag='035', indicators=['\\', '\\'], subfields=['a', record['024']['a']], marctype="isni"))
 
@@ -187,3 +234,53 @@ class MARC21ToISNIMARC:
         newrecord.add_field(Field(tag='922', indicators=['\\', '\\'], subfields=['a', self.countrycode], marctype="isni"))
         return newrecord
 
+    def makeIsniRequest(self, record):
+
+        """
+        requestdict = {"requestorIdentifierOfIdentity": {"identifier": "", "type": "", "otheridentifierOfIdentity":{ "identifier": "", "type": ""}},
+                       "personalName": {"nameUse": "", "personalName": "", "numeration": "", "nameTitle":""},
+                       "birthDate": "", "deathDate": "", "nationality": "", "personalNameVariant": "", "instrumentAndVoice": "",
+                       "location": {"countryCode": "", "locode": "", "regionOrState": "", "city": ""},
+                       "countriesAssociated": {"countryCode": "", "regionOrState": "", "city": ""},
+                       "externalInformation": {"source": "", "information": "", "URI": ""},
+                       "LanguageOfIdentity": "",
+                       "isRelated": {"relationType": "", "hasMember": {}, "hasEmployee": {}, "supersedes": {}, "isAffiliatedWith": {}, "relationQualification": "",
+                                     "startDateOfRelationship": "", "endDateOfRelationship": ""},
+                       "isNot": {"identityType": "", "PPN":{"personalName": "", "organisationName": ""}},
+                       "merge": "",
+                       "titleOfWork": {"title": "", "subtitle": "", "Publisher": "", "dateOfPublication": "", "resourceIdentifier":{
+                           "identifier": "", "identifierValue": "", "identifierType": ""
+                       }, "creationClass": {}, "creationRole": {}, "fieldOfCreation": {"fieldType": {}, "fieldOfCreationValue": ""}, "contributedTo": {
+                           "title": "", "identifier": "", "identifierValue": "", "identifierType": ""
+                       }}}
+                       """
+        requestdict = {"Request": {}}
+        for field in record.fields:
+            if field.tag == '024':
+                requestdict["Request"]["requestorIdentifierOfIdentity"] = {"otheridentifierOfIdentity": {"identifier": record['024']['a']}}
+            elif field.tag == '035':
+                requestdict["Request"]["requestorIdentifierOfIdentity"] = {"identifier": record['035']['a']}
+            elif field.tag == '110':
+                requestdict["Request"]["organisationName"] = {"mainName": record['110']['a']}
+                if record['110']['b']:
+                    requestdict["Request"]["organisationName"] = {"subdivisionName": record['110']['b']}
+            elif field.tag == '368':
+                requestdict["Request"]["organisationType"] = record['368']['a']
+            elif field.tag == '046':
+                if record['046']['s']:
+                    requestdict["Request"]["usageDateFrom"] = record['046']['s']
+                if record['046']['t']:
+                    requestdict["Request"]["usageDateTo"] = record['046']['t']
+                if record['046']['q']:
+                    requestdict["Request"]["usageDateFrom"] = record['046']['q']
+                if record['046']['r']:
+                    requestdict["Request"]["usageDateTo"] = record['046']['r']
+            elif field.tag == '410':
+                requestdict["Request"]["organisationName"]["organisationNameVariant"] = record['410']['a']
+            elif field.tag == '411':
+                requestdict["Request"]["organisationName"]["organisationNameVariant"] = record['411']['a']
+            elif field.tag == '370':
+                if record['370']['e']:
+                    requestdict["Request"]["location"] = {"countryCode": record['370']['e']}
+
+        return requestdict
