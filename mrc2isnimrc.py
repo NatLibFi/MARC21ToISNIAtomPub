7a21022f6f8e68fa8008827ab8926c8deccf921e
Anis <anis.moubarik@gmail.com>
2017-10-02 09:59:15 +0300

    fix isRelated field and remove marc to isnimarc binary conversion

--- mrc2isnimrc.py
+++ mrc2isnimrc.py
@@ -164,172 +164,6 @@ class MARC21ToISNIMARC:
 
             print("Conversion done")
 
-    def makeRecord(self, record):
-        """
-        Converts the record the pymarcs MARCReader has read to ISNIMARC format.
-        Takes a whole record as a parameter and returns the converted record.
-        :param record: Old MARC21 Record
-        :return newrecord: New ISNIMARC Record
-        """
-        newrecord = Record(force_utf8=True, to_unicode=True)
-        for field in record.fields:
-            if field.tag == '024':
-                newrecord.add_field(
-                    Field(tag='035', indicators=['\\', '\\'], subfields=['a', record['024']['a']], marctype="isni"))
-
-            elif field.tag == '110':
-                newrecord.add_field(
-                    Field(tag='710', indicators=['2', '\\'], subfields=['a', record['110']['a']], marctype="isni"))
-                if record['110']['b']:
-                    newrecord.add_field(
-                        Field(tag='710', indicators=['2', '\\'], subfields=['b', record['110']['b']], marctype="isni"))
-
-            elif field.tag == '111':
-                newrecord.add_field(
-                    Field(tag='710', indicators=['\\', '\\'], subfields=['a', record['111']['a']], marctype="isni"))
-                if record['111']['e']:
-                    newrecord.add_field(
-                        Field(tag='710', indicators=['\\', '\\'], subfields=['a', record['111']['e']], marctype="isni"))
-
-            elif field.tag == '046':
-                if record['046']['s']:
-                    newrecord.add_field(
-                        Field(tag='970', indicators=['\\', '\\'], subfields=['a', record['046']['s']], marctype="isni"))
-                if record['046']['t']:
-                    newrecord.add_field(
-                        Field(tag='970', indicators=['\\', '\\'], subfields=['b', record['046']['t']], marctype="isni"))
-                if record['046']['q']:
-                    newrecord.add_field(
-                        Field(tag='970', indicators=['\\', '\\'], subfields=['a', record['046']['q']], marctype="isni"))
-                if record['046']['r']:
-                    newrecord.add_field(
-                        Field(tag='970', indicators=['\\', '\\'], subfields=['b', record['046']['r']], marctype="isni"))
-                if record['046']['f']:
-                    newrecord.add_field(
-                        Field(tag='970', indicators=['\\', '\\'], subfields=['a', record['046']['f']], marctype="isni"))
-                if record['046']['g']:
-                    newrecord.add_field(
-                        Field(tag='970', indicators=['\\', '\\'], subfields=['b', record['046']['g']], marctype="isni"))
-
-            elif field.tag == '411':
-                if record['411']['a']:
-                    newrecord.add_field(
-                        Field(tag='410', indicators=['\\', '\\'], subfields=['a', record['411']['a']], marctype="isni"))
-                if record['411']['b']:
-                    newrecord.add_field(
-                        Field(tag='410', indicators=['\\', '\\'], subfields=['a', record['411']['b']], marctype="isni"))
-
-            elif field.tag == '370':
-                if record['370']['e']:
-                    newrecord.add_field(
-                        Field(tag='370', indicators=['\\', '\\'], subfields=['a', record['370']['e']], marctype="isni"))
-                if record['370']['c']:
-                    newrecord.add_field(
-                        Field(tag='922', indicators=['\\', '\\'], subfields=['a', record['370']['c']], marctype="isni"))
-                if record['370']['e']:
-                    newrecord.add_field(
-                        Field(tag='922', indicators=['\\', '\\'], subfields=['z', record['370']['e']], marctype="isni"))
-
-            elif field.tag == '670':
-                if record['670']['a']:
-                    newrecord.add_field(
-                        Field(tag='670', indicators=['\\', '\\'], subfields=['a', record['670']['a']], marctype="isni"))
-                if record['670']['b']:
-                    newrecord.add_field(
-                        Field(tag='670', indicators=['\\', '\\'], subfields=['b', record['670']['b']], marctype="isni"))
-                if record['670']['u']:
-                    newrecord.add_field(
-                        Field(tag='670', indicators=['\\', '\\'], subfields=['u', record['670']['u']], marctype="isni"))
-
-            elif field.tag == '377':
-                if record['377']['a']:
-                    newrecord.add_field(
-                        Field(tag='377', indicators=['\\', '\\'], subfields=['a', record['377']['a']], marctype="isni"))
-                if record['377']['l']:
-                    newrecord.add_field(
-                        Field(tag='670', indicators=['\\', '\\'], subfields=['a', record['670']['l']], marctype="isni"))
-
-            elif field.tag == '510':
-                if record['510']['a']:
-                    newrecord.add_field(
-                        Field(tag='951', indicators=['\\', '\\'], subfields=['t', record['510']['a']], marctype="isni"))
-                if record['510']['b']:
-                    newrecord.add_field(
-                        Field(tag='951', indicators=['\\', '\\'], subfields=['t', record['510']['b']], marctype="isni"))
-
-            elif field.tag == '020':
-                newrecord.add_field(
-                    Field(tag='901', indicators=['\\', '\\'], subfields=['a', record['020']['a']], marctype="isni"))
-
-            elif field.tag == '022':
-                newrecord.add_field(
-                    Field(tag='902', indicators=['\\', '\\'], subfields=['a', record['022']['a']], marctype="isni"))
-
-            elif field.tag == '024':
-                newrecord.add_field(
-                    Field(tag='904', indicators=['\\', '\\'], subfields=['a', record['024']['a']], marctype="isni"))
-
-            elif field.tag == '245':
-                if record['245']['a']:
-                    newrecord.add_field(
-                        Field(tag='910', indicators=['\\', '\\'], subfields=['a', record['245']['a']], marctype="isni"))
-                if record['245']['b']:
-                    newrecord.add_field(
-                        Field(tag='910', indicators=['\\', '\\'], subfields=['b', record['245']['b']], marctype="isni"))
-
-            elif field.tag == '260':
-                if record['260']['c']:
-                    newrecord.add_field(
-                        Field(tag='943', indicators=['\\', '\\'], subfields=['a', record['260']['c']], marctype="isni"))
-
-            elif field.tag == '264':
-                if record['264']['c']:
-                    newrecord.add_field(
-                        Field(tag='943', indicators=['\\', '\\'], subfields=['a', record['264']['c']], marctype="isni"))
-
-            elif field.tag == '336':
-                newrecord.add_field(
-                    Field(tag='944', indicators=['\\', '\\'], subfields=['a', record['336']['a']], marctype="isni"))
-
-            elif field.tag == '110':
-                if record['110']['e']:
-                    newrecord.add_field(
-                        Field(tag='941', indicators=['\\', '\\'], subfields=['a', record['110']['e']], marctype="isni"))
-
-            elif field.tag == '710':
-                if record['710']['e']:
-                    newrecord.add_field(
-                        Field(tag='941', indicators=['\\', '\\'], subfields=['a', record['710']['e']], marctype="isni"))
-
-            elif field.tag == '260':
-                if record['260']['b']:
-                    newrecord.add_field(
-                        Field(tag='921', indicators=['\\', '\\'], subfields=['a', record['260']['b']], marctype="isni"))
-
-            elif field.tag == '264':
-                if record['264']['b']:
-                    newrecord.add_field(
-                        Field(tag='921', indicators=['\\', '\\'], subfields=['a', record['264']['b']], marctype="isni"))
-
-            elif field.tag == '100':
-                if record['100']['a']:
-                    newrecord.add_field(
-                        Field(tag='700', indicators=['\\', '\\'], subfields=['a', record['100']['a']], marctype="isni"))
-                if record['100']['b']:
-                    newrecord.add_field(
-                        Field(tag='700', indicators=['\\', '\\'], subfields=['b', record['100']['b']], marctype="isni"))
-                if record['100']['c']:
-                    newrecord.add_field(
-                        Field(tag='700', indicators=['\\', '\\'], subfields=['c', record['100']['c']], marctype="isni"))
-
-            else:
-                newrecord.add_field(field)
-
-        ##Country field is obligatory for ISNIMARC
-        newrecord.add_field(
-            Field(tag='922', indicators=['\\', '\\'], subfields=['a', self.countrycode], marctype="isni"))
-        return newrecord
-
     def makeIsniRequest(self, record):
 
         requestIdentifier = []
@@ -385,19 +219,19 @@ class MARC21ToISNIMARC:
         for t in record.get_fields("510"):
             isRelated = {}
             if t['w'] is "a":
-                isRelated = {"relationType": "supersedes", "identityType": "organisation", "noISNI": {"PPN": "", "organisationName": t['a']}}
+                isRelated = {"relationType": "supersedes", "identityType": "organisation", "relationName": {"PPN": "", "organisationName": t['a']}}
                 if t['b']:
-                    isRelated.update({"noISNI": {"PPN": "", "organisationName": t['a'], "subDivisionName": t['b']}})
+                    isRelated.update({"relationName": {"PPN": "", "organisationName": t['a'], "subDivisionName": t['b']}})
                 relations.append(isRelated)
             elif t['w'] is "b":
-                isRelated = {"relationType": "isSupersededBy", "identityType": "organisation", "noISNI": {"PPN": "", "organisationName": t['a']}}
+                isRelated = {"relationType": "isSupersededBy", "identityType": "organisation", "relationName": {"PPN": "", "organisationName": t['a']}}
                 if t['b']:
-                    isRelated.update({"noISNI": {"PPN": "", "organisationName": t['a'], "subDivisionName": t['b']}})
+                    isRelated.update({"relationName": {"PPN": "", "organisationName": t['a'], "subDivisionName": t['b']}})
                 relations.append(isRelated)
             elif t['w'] is "t":
-                isRelated = {"relationType": "isUnitOf", "identityType": "organisation", "noISNI": {"PPN": "", "organisationName": t['a']}}
+                isRelated = {"relationType": "isUnitOf", "identityType": "organisation", "relationName": {"PPN": "", "organisationName": t['a']}}
                 if t['b']:
-                    isRelated.update({"noISNI": {"PPN": "", "organisationName": t['a'], "subDivisionName": t['b']}})
+                    isRelated.update({"relationName": {"PPN": "", "organisationName": t['a'], "subDivisionName": t['b']}})
                 relations.append(isRelated)
 
         for t in record.get_fields("400"):
@@ -556,16 +390,13 @@ class MARC21ToISNIMARC:
                 idtypexml.text = c['identityType']
                 relationtypexml = ET.SubElement(isrelatedxml, "relationType")
                 relationtypexml.text = c['relationType']
-                noisnixml = ET.SubElement(isrelatedxml, "noISNI")
-                # ppnxml = ET.SubElement(noisnixml, "PPN")
-                # ppnxml.text = ""
-                relorgnamexml = ET.SubElement(noisnixml, "organisationName")
+                relationnamexml = ET.SubElement(isrelatedxml, "relationName")
+                relorgnamexml = ET.SubElement(relationnamexml, "organisationName")
                 relorgmainxml = ET.SubElement(relorgnamexml, "mainName")
-                relorgmainxml.text = c['noISNI']['organisationName']
-                if c['noISNI'].get('subDivisionName') is not None:
+                relorgmainxml.text = c['relationName']['organisationName']
+                if c['relationName'].get('subDivisionName') is not None:
                     subdivnamexml = ET.SubElement(relorgnamexml, "subdivisionName")
-                    subdivnamexml.text = c['noISNI']['subDivisionName']
-
+                    subdivnamexml.text = c['relationName']['subDivisionName']
 
         if organisationVariants:
             for c in organisationVariants:
@@ -611,7 +442,8 @@ class MARC21ToISNIMARC:
 
         return requestxml
 
-    def converttoiso(self, country):
+    @staticmethod
+    def converttoiso(country):
         convertdict = {"afganistan": "AF", "ahvenanmaa": "AX", "alankomaat": "NL", "albania": "AL", "algeria": "DZ", "andorra": "AD",
                        "angola": "AO", "arabiemiirikunnat": "AE", "argentiina": "AR", "armenia": "AM", "australia": "AU", "bahrain": "BH",
                        "bahama": "BS", "bangladesh": "BD", "barbados": "BB", "belgia": "BE", "belize": "BZ", "bolivia": "BO", "bosnia ja hertsegovina": "BA",
